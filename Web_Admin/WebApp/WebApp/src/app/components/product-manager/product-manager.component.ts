085adf3a38bc17459f14c9afa0a5fbf00d552eb3
Erick Barrantes <erick.cr12@outlook.com>
2021-06-20 20:59:55 -0600

    Product management done.

--- Web_Admin/WebApp/WebApp/src/app/components/product-manager/product-manager.component.ts
+++ Web_Admin/WebApp/WebApp/src/app/components/product-manager/product-manager.component.ts
@@ -9,27 +9,48 @@ import { DataService } from '../../data.service';
 })
 export class ProductManagerComponent implements OnInit {
 
-  //deviceTypes: Productos[];
-  //private dataService: DataService
+  productos: Producto[] = [];
+  productoSeleccionado: Producto = {} as Producto;
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-    //this.getProductos();
+    this.getProductos();
   }
+
+  onChange(idProductoStr: string): void{
+    this.productoSeleccionado = this.productos.find(x => x.barCode === Number(idProductoStr));
+  }
+
 
   getProductos(): void {
-    //this.dataService.getAllProductos().subscribe( data => this.Productos = data);
+    this.dataService.getAllProductos().subscribe( data => this.productos = data);
   }
 
-  addProduct(name: string, description:string , barCode: number, price: number): void {
-    
-    const newProd = { barCode, name, description, price } as Producto;
-
+  addProduct(name: string, description: string, barCodeStr: string, priceStr: string): void {
+    const barCode = Number(barCodeStr);
+    const price = Number(priceStr);
+    const newProd = {barCode, name, description, price} as Producto;
+    console.log(newProd);
     this.dataService.addProducto(newProd).subscribe(data => {
       if (data) {
-        this.getProductos
+        this.getProductos();
       }
     });
+  }
+
+  modificarProducto(name: string, description: string, barCodeStr: string, priceStr: string): void{
+    const barCode = Number(barCodeStr);
+    const price = Number(priceStr);
+    const PLanillaporModificar = {barCode, name, description, price} as Producto;
+    this.dataService.updateProducto(PLanillaporModificar).subscribe(data => {
+      this.getProductos();
+    });
+  }
+
+  eliminarProducto(barCodeStr: string): void{
+    const barCode = Number(barCodeStr);
+    this.dataService.deleteProducto(barCode).subscribe();
+    this.productos = this.productos.filter(x => x.barCode !== barCode);
 
   }
 
